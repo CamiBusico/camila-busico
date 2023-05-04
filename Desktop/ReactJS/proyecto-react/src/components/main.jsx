e93d44849b96f048550a6a1fe952981188397b6f
Camila Busico <camilabusico@gmail.com>
2023-05-04 15:25:02 -0300

    commit2

--- Desktop/ReactJS/proyecto-react/src/components/main.jsx
+++ Desktop/ReactJS/proyecto-react/src/components/main.jsx
@@ -4,11 +4,15 @@ const Main  = () => {
             <h1>Descrubí la mejor cerveza artesanal</h1>
 
             <div className="cardsCervezas">
-                   
+                        <div className="card" style="width: 18rem;">
+                            <img src="..." className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                         </div>
              </div>
-        
-        
-        
         
         </div>
     )
